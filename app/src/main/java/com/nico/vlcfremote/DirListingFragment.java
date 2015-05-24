40e7b74a11d19a2487be7912d2a8a77fc2600832
Nico <nicolasbrailo@gmail.com>
2015-05-24 23:05:19 +0200

    Adding dir bookmark

--- app/src/main/java/com/nico/vlcfremote/DirListingFragment.java
+++ app/src/main/java/com/nico/vlcfremote/DirListingFragment.java
@@ -1,9 +1,13 @@
 package com.nico.vlcfremote;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,17 +16,23 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nico.vlcfremote.utils.VlcActionFragment;
 import com.nico.vlcfremote.utils.VlcConnector;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class DirListingFragment extends VlcActionFragment implements View.OnClickListener {
 
     // Is this Windows compatible? Who knows...
     private static final String VLC_DEFAULT_START_PATH = "~";
+
+    // A char that can never appear in a path, used to sepparate two paths in a setting
+    private static final char BOOKMARK_PATHS_SEPARATOR = '|';
 
     String currentPath;
     String currentPath_display;
@@ -33,6 +43,8 @@ public class DirListingFragment extends VlcActionFragment implements View.OnClic
         View v = inflater.inflate(R.layout.fragment_dir_listing, container, false);
         dirViewAdapter = new DirListEntry_ViewAdapter(this, getActivity());
         ((ListView) v.findViewById(R.id.wDirListing_List)).setAdapter(dirViewAdapter);
+        v.findViewById(R.id.wDirListing_Bookmark).setOnClickListener(this);
+        v.findViewById(R.id.wDirListing_JumpToBookmark).setOnClickListener(this);
         currentPath = VLC_DEFAULT_START_PATH;
         currentPath_display = getResources().getString(R.string.dir_listing_default_folder_label);
         return v;
@@ -106,9 +118,73 @@ public class DirListingFragment extends VlcActionFragment implements View.OnClic
 
                 break;
 
+            case R.id.wDirListing_Bookmark:
+                saveBookmark(this.currentPath_display, this.currentPath);
+                break;
+
+            case R.id.wDirListing_JumpToBookmark:
+                displayBookmarkPicker();
+                break;
+
             default:
                 throw new RuntimeException(DirListingFragment.class.getName() + " received a click event it can't handle.");
         }
+    }
+
+    private void saveBookmark(final String pathDisplayName, final String pathUri) {
+        if (!isAdded()) return;
+        final FragmentActivity activity = getActivity();
+
+        // Bookmarks are kept per server
+        final String prefKey = vlcConnection.getVlcConnector().getServerUrl() + "_dirBookmarks";
+
+        // Get the known bookmarks for the current server
+        Set<String> bookmarks = activity.getPreferences(Context.MODE_PRIVATE).getStringSet(prefKey, new HashSet<String>());
+
+        bookmarks.add(pathUri + BOOKMARK_PATHS_SEPARATOR + pathDisplayName);
+        SharedPreferences.Editor cfg = activity.getPreferences(Context.MODE_PRIVATE).edit();
+        cfg.putStringSet(prefKey, bookmarks);
+        cfg.apply();
+        cfg.commit();
+
+        final String msg = String.format(getResources().getString(R.string.dir_listing_saved_bookmark), pathDisplayName);
+        Toast toast = Toast.makeText(activity.getApplicationContext(), msg, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    private void displayBookmarkPicker() {
+        if (!isAdded()) return;
+        final FragmentActivity activity = getActivity();
+
+        final String prefKey = vlcConnection.getVlcConnector().getServerUrl() + "_dirBookmarks";
+        Set<String> bookmarks = activity.getPreferences(Context.MODE_PRIVATE).getStringSet(prefKey, new HashSet<String>());
+
+        final List<String> pathDisplayNames = new ArrayList<>();
+        final List<String> pathUris = new ArrayList<>();
+        for (String bookmark : bookmarks) {
+            final String pathUri = bookmark.substring(0, bookmark.indexOf(BOOKMARK_PATHS_SEPARATOR));
+            final String pathDisplayName = bookmark.substring(bookmark.indexOf(BOOKMARK_PATHS_SEPARATOR)+1);
+            pathUris.add(pathUri);
+            pathDisplayNames.add(pathDisplayName);
+            Log.i("ASDASDA", pathUri + " -> " + pathDisplayName);
+        }
+
+        Log.i("ASDASDA", String.valueOf(pathDisplayNames.toArray(new String[pathDisplayNames.size()])));
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle(activity.getString(R.string.R_string_dir_listing_goto_bookmark_title));
+        builder.setItems(pathDisplayNames.toArray(new String[pathDisplayNames.size()]), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // the user clicked on colors[which]
+                Log.i("ASDASDA", pathUris.get(which));
+
+                currentPath = pathUris.get(which);
+                currentPath_display = pathDisplayNames.get(which);
+                updateDirectoryList();
+            }
+        });
+        builder.show();
     }
 
     private static class DirListEntry_ViewAdapter extends ArrayAdapter<VlcConnector.DirListEntry> {
